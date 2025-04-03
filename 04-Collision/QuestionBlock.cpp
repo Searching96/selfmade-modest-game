aadcceb28ccf86809c6d0ec26139c2874e0239da
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-04-03 18:03:36 +0700

    add question block class

--- 04-Collision/QuestionBlock.cpp
+++ 04-Collision/QuestionBlock.cpp
@@ -1,1 +1,47 @@
 #include "QuestionBlock.h"
+#include "QuestionBlock.h"
+#include "Star.h"
+#include "Game.h"
+
+void CQuestionBlock::Render()
+{
+	int aniId = ID_ANI_QUESTION_BLOCK;
+	if (state == QUESTION_BLOCK_STATE_HIT)
+	{
+		aniId = ID_ANI_QUESTION_BLOCK_HIT;
+	}
+
+	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void CQuestionBlock::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
+void CQuestionBlock::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - QUESTION_BLOCK_BBOX_WIDTH / 2;
+	t = y - QUESTION_BLOCK_BBOX_HEIGHT / 2;
+	r = l + QUESTION_BLOCK_BBOX_WIDTH;
+	b = t + QUESTION_BLOCK_BBOX_HEIGHT;
+}
+
+void CQuestionBlock::SetState(int state)
+{
+	CGameObject::SetState(state);
+}
+
+void CQuestionBlock::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+	if (e->ny > 0 && state == QUESTION_BLOCK_STATE_NOT_HIT)
+	{
+		SetState(QUESTION_BLOCK_STATE_HIT);
+
+		CGame* game = CGame::GetInstance();
+		LPGAMEOBJECT star = new CStar(x, y - STAR_BBOX_HEIGHT / 2);
+		//add game object
+	}
+}
--- 04-Collision/QuestionBlock.h
+++ 04-Collision/QuestionBlock.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "GameObject.h"
 
+#include <vector>
+
 #define QUESTION_BLOCK_BBOX_WIDTH  16
 #define QUESTION_BLOCK_BBOX_HEIGHT 16
+
+#define ID_ANI_QUESTION_BLOCK 30000
+#define ID_ANI_QUESTION_BLOCK_HIT 31000
+
+#define QUESTION_BLOCK_STATE_NOT_HIT 100
+#define QUESTION_BLOCK_STATE_HIT 200
 
 class CQuestionBlock : public CGameObject
 {
@@ -13,8 +21,9 @@ public:
 	CQuestionBlock(float x, float y) : CGameObject(x, y) {}
 
 	void Render();
-	void Update(DWORD dt, vector<LPGAMEOBJECT*> coObjects);
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	void SetState(int state);
+	void OnCollisionWith(LPCOLLISIONEVENT e);
 };
 
